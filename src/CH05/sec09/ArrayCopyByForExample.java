da73b87b3ac465b4ae05cc5167dc2d39ed4c598d
Lyu0010 <202211020@sangmyung.kr>
2025-10-15 10:42:10 +0900

    7주차 실습

---
+++ src/CH05/sec09/ArrayCopyByForExample.java
@@ -0,0 +1,15 @@
+package CH05.sec09;
+
+public class ArrayCopyByForExample {
+    public static void main(String[] args) {
+        //길이 3인 배열
+        int[] oldIntArray = { 1, 2, 3 };
+        int[] newIntArray = new int[5];
+        for(int i=0; i<oldIntArray.length; i++) {
+            newIntArray[i] = oldIntArray[i];
+        }
+        for(int i=0; i<newIntArray.length; i++) {
+            System.out.print(newIntArray[i] + ", ");
+        }
+    }
+}
---
+++ src/CH05/sec12/Week.java
@@ -0,0 +1,11 @@
+package CH05.sec12;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
