ab34b06e4a7499576d44435d4a13f51fcc7a8c6c
Lyu0010 <202211020@sangmyung.kr>
2025-11-19 12:19:22 +0900

    12주차 과제

---
+++ src/CH06/sec13/exam02/package1/B.java
@@ -0,0 +1,10 @@
+package CH06.sec13.exam02.package1;
+
+public class B {
+    //필드 선언
+    A a1 = new A(true); //O
+    A a2 = new A(1);    //O
+    A a3 = new A("문자열"); //X
+    
+    // 이 이후의 코드는 이미지에 보이지 않습니다.
+}
